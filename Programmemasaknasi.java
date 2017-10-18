48897526a75a90ee246f42b19ba6ca8938c19fb1
dhaniachmadrainaldi <32839233+dhaniachmadrainaldi@users.noreply.github.com>
2017-10-18 20:10:17 +0700

    Add files via upload
    Tugas 1 Java

---
+++ Programmemasaknasi.java
@@ -0,0 +1,16 @@
+\\package program.memasak.nasi
+
+public class Program memasak nasi{
+public static void main {string[]args}{
+\\ TODO code application logic here
+
+system.out.println("Cara memasak nasi");
+system.out.println("siapkan alat");
+system.out.println("siapkan bahan");
+system.out.println("cuci beras");
+system.out.println("masukan beras dan tuangkan air secukupnya ke ricecookeer");
+system.out.println("nyalakan rice cooker");
+system.out.println("Tunggu nasi hingga matang");
+system.out.println("Nasi siap dihidangkan");
+}
+}
